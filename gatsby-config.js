032eabcb8d5e1c0cd52b6e2ba7ca4d95aa6f5ac9
kevinwky2022 <113677550+kevinwky2022@users.noreply.github.com>
2022-11-25 00:27:11 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     siteTitle: 'Workouts Map',
     siteUrl: 'https://ben29.xyz',
-    logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+    logo: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/108302326/25575907/4/large.jpg',
     description: 'Personal Workouts Page',
     keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
     navLinks: [
